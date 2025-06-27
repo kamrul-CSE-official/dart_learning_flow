5c87b37223f4aab1537e95e720800c86d670c619
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-06-27 12:08:15 -0700

    Class-4 done

--- bin/classes/class4.dart
+++ bin/classes/class4.dart
@@ -1,5 +1,36 @@
 void lesson() {
   List listOfValues = [12, 22, 34, 87, 34, 98, 11, "kamrul", 30.22, true];
 
+  print("list lenght: ${listOfValues.length}");
+  print(listOfValues[4]);
+
+  listOfValues.add("Hasan");
+  listOfValues.add(12.34);
+  listOfValues.insert(1, "Good");
   print(listOfValues);
+  listOfValues.remove("Good");
+  listOfValues.removeAt(7);
+  listOfValues[1] = 33;
+
+  print(listOfValues);
+
+  List<int> numbers = [23, 24, 87];
+  print(numbers);
+
+  Map teacher = {"name": "Hamid", "age": 48, "CGPA": 3.45};
+  print(teacher);
+  teacher["address"] = "Dhaka";
+  teacher.remove("CGPA");
+  teacher["age"] = 50;
+
+  print(teacher.keys);
+  print(teacher.values);
+  print(teacher);
+
+  List<Map> students = [
+    {"name": "MD.Kamrul Hasan", "age": 25},
+    {"name": "MD. Hasan", "age": 29},
+    {"name": "MD. Shobo", "age": 26},
+  ];
+  print(students);
 }
