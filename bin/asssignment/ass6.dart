d462a94429c35cae17768aaff3612699b06ec5c0
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-07-05 00:54:55 +0600

    Assignment-6 done

---
+++ bin/asssignment/ass6.dart
@@ -0,0 +1,26 @@
+void ass6() {
+  // No Return type & no argument function
+  void studentName() {
+    print("MD.kamrul Hasan");
+  }
+
+  // With return type & argument function
+  int yourAge(int age) {
+    return age;
+  }
+
+  // No Return type with argument function
+  void yourRool(int rool) {
+    print("Your rool is: $rool");
+  }
+
+  // with return type with argument function
+  String yourFacthersName(String name) {
+    return "Your father $name";
+  }
+
+  studentName();
+  yourAge(25);
+  yourRool(12);
+  yourFacthersName("MD.Maksub");
+}
